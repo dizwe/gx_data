212c31a88e07588ae968bf1f2b17d0bf39a9b7cf
dizwe <dizwe2716@gmail.com>
2019-08-06 14:43:18 +0900

    twitter_trend python으로 옮긴것 파일 날짜별로 자를 수 있게 하기

--- twitter/twitter_trends.py
+++ twitter/twitter_trends.py
@@ -8,8 +8,7 @@ auth_header = {
   'access_token_key': '1127968910-XAALt7MLvPZuQzRS33Ksf4RtzaOBy6rXSBMzcHD',
   'access_token_secret': os.environ['ACCESSTOKEN_SECRET'],
 }
-START = '2019-07-25'
-END = '2019-08-02'
+
 q = 'klaytn'
 # head = {'authorization':'OAuth oauth_consumer_key'}
 # requests.get('https://api.twitter.com/1.1/',)
@@ -27,71 +26,94 @@ def oauth_req(url, key, secret, http_method="GET", post_body=b"", http_headers=N
 
 #%%
 import json
-# {q: q,max_id:next_page_max_id,since_id:since_id,count:100}
-tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&until={START}',
-                         auth_header['access_token_key'],
-                         auth_header['access_token_secret'])
-# bytes로 받아져서 수정해줘야 함
-tweets = json.loads(tweets_bytes.decode('utf8'))
-
-#%%
-##### 시작점 받기
-since_id=0
-if len(tweets['statuses'])>0: # 아예 tweets이 없을 수도 있음.
-    since_id =  tweets['statuses'][0]['id']
-    print('created_at',tweets['statuses'][0]['created_at'])
-    print('since_id', since_id)
-    # return client.get('search/tweets', {q: q,since_id:since_id, until:end, count:100});
-
-#%%
+from datetime import datetime,timedelta
 from functools import reduce
 import re
-raw_data = []
-retweet_num, favorite_num, tweets_num = 0,0,0
-if since_id != 0: # 빈 내용이 아니라면
-    tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&since_id={since_id}&until={END}&count=100',
-                        auth_header['access_token_key'],
-                        auth_header['access_token_secret'])
+now =datetime.now()
+
+# 5일 데이터보기
+for i in range(5):
+# def extract_twt_data(
+#     twt_auth, 
+#     twt_query, 
+#     execute_date,
+#     output):
+    # 오늘 날짜 뺴고 체크
+    START = (now - timedelta(days=i+2)).strftime("%Y-%m-%d")
+    END = (now - timedelta(days=i+1)).strftime("%Y-%m-%d")
+
+    #%%
+    # {q: q,max_id:next_page_max_id,since_id:since_id,count:100}
+    tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&until={START}',
+                            auth_header['access_token_key'],
+                            auth_header['access_token_secret'])
+    # bytes로 받아져서 수정해줘야 함
     tweets = json.loads(tweets_bytes.decode('utf8'))
-    statuses = tweets['statuses']
 
-    while len(statuses)>0: # 페이지 끝날때까지 확인!
-        ## 트윗 확인
-        # for status in tweets['statuses']:
-        #     print(status.get('text',''))
+    #%%
+    ##### 시작점 받기
+    since_id=0
+    if len(tweets['statuses'])>0: # 아예 tweets이 없을 수도 있음.
+        since_id =  tweets['statuses'][0]['id']
+        print('created_at',tweets['statuses'][0]['created_at'])
+        print('since_id', since_id)
+        # return client.get('search/tweets', {q: q,since_id:since_id, until:end, count:100});
 
-        raw_data += statuses
-        # 몇개 트윗 나왔는지 알아보기(리트윗 포함)
-        tweets_num += len(statuses)
-
-        # 리트윗 몇번 일어나는지 확인하기 
-        def retweet_reducer(acc,cur):
-            # 리트윗 한게 아닐때만 카운트하기, 이렇게 안하면 리트윗 한것들 모두가 본 글 리트윗 수를 가져서 정보가 잘못된다.
-            # 좋아요는 리트윗 한건 따로 count 되어서 괜찮음
-            if('retweeted_status' not in cur):
-                return acc + cur['retweet_count']
-            else:
-                return acc
-        
-        retweet_num += reduce(retweet_reducer, statuses, 0)
-        favorite_num += reduce(lambda acc,cur: acc + cur['favorite_count'],statuses,0)
-
-        print("-----------START------")
-        print(statuses[0]['id'])
-        print("-----------END------")
-        print(statuses[-1]['id'])
-
-        # 다음 페이지 읽어오기
-        t = re.findall("max_id=([0-9]+)&",tweets['search_metadata']['next_results'])[0]
-        new_next_page_max_id  = int(re.findall("[0-9]+", t)[0])
-        print("-----------NEXT------")
-        print(new_next_page_max_id)
-        tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&since_id={since_id}&max_id={new_next_page_max_id}&until={END}&count=100',
-                                auth_header['access_token_key'],
-                                auth_header['access_token_secret'])
+    #%%
+    raw_data = []
+    retweet_num, favorite_num, tweets_num = 0,0,0
+    if since_id != 0: # 빈 내용이 아니라면
+        tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&since_id={since_id}&until={END}&count=100',
+                            auth_header['access_token_key'],
+                            auth_header['access_token_secret'])
         tweets = json.loads(tweets_bytes.decode('utf8'))
         statuses = tweets['statuses']
-    print('retweet_num, favorite_num, tweets_num')
-    print(retweet_num, favorite_num, tweets_num)
-    
+
+        while len(statuses)>0: # 페이지 끝날때까지 확인!
+            ## 트윗 확인
+            # for status in tweets['statuses']:
+            #     print(status.get('text',''))
+
+            raw_data += statuses
+            # 몇개 트윗 나왔는지 알아보기(리트윗 포함)
+            tweets_num += len(statuses)
+
+            # 리트윗 몇번 일어나는지 확인하기 
+            def retweet_reducer(acc,cur):
+                # 리트윗 한게 아닐때만 카운트하기, 이렇게 안하면 리트윗 한것들 모두가 본 글 리트윗 수를 가져서 정보가 잘못된다.
+                # 좋아요는 리트윗 한건 따로 count 되어서 괜찮음
+                if('retweeted_status' not in cur):
+                    return acc + cur['retweet_count']
+                else:
+                    return acc
+            
+            retweet_num += reduce(retweet_reducer, statuses, 0)
+            favorite_num += reduce(lambda acc,cur: acc + cur['favorite_count'],statuses,0)
+
+            print("-----------START------")
+            print(statuses[0]['id'])
+            print("-----------END------")
+            print(statuses[-1]['id'])
+
+            # 다음 페이지 읽어오기
+            t = re.findall("max_id=([0-9]+)&",tweets['search_metadata']['next_results'])[0]
+            new_next_page_max_id  = int(re.findall("[0-9]+", t)[0])
+            print("-----------NEXT------")
+            print(new_next_page_max_id)
+            tweets_bytes = oauth_req(f'https://api.twitter.com/1.1/search/tweets.json?q={q}&since_id={since_id}&max_id={new_next_page_max_id}&until={END}&count=100',
+                                    auth_header['access_token_key'],
+                                    auth_header['access_token_secret'])
+            tweets = json.loads(tweets_bytes.decode('utf8'))
+            statuses = tweets['statuses']
+        print('retweet_num, favorite_num, tweets_num')
+        print(retweet_num, favorite_num, tweets_num) 
+
+    #%%
+    # 날짜별로 파일 구분하고 데이터 얻어오기
+    with open(f'twitter_raw_data-{START}.file','w') as a:
+        # 나중에 dump하기 쉽게 한 줄로 만들기
+        for one_data in raw_data:
+            a.write(json.dumps(one_data))
+            a.write('\n')
+
 #%%
